ee3c5a57c0fadf94d8899b5d8b68f16a6aaa7dba
Bogdan Kostadinov <bogdan.kostadinov1@gmail.com>
2025-04-26 00:33:58 +0100

    team repository

---
+++ Repositories/TeamRepository.cs
@@ -0,0 +1,31 @@
+﻿using DotaNerf.Data;
+using DotaNerf.Entities;
+using DotaNerf.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotaNerf.Repositories;
+
+public class TeamRepository : ITeamRepository
+{
+    private readonly DataContext _context;
+    public TeamRepository(DataContext context)
+    {
+        _context = context;
+    }
+    public async Task<Team> GetTeamAsync(Guid id)
+    {
+        var team = await _context.Teams.FirstOrDefaultAsync(t => t.Id == id);
+        if (team == null)
+        {
+            throw new KeyNotFoundException($"Team with ID {id} not found.");
+        }
+        return team;
+    }
+    public async Task DeleteTeamAsync(Guid id)
+    {
+        var team = GetTeamAsync(id).Result;
+
+        _context.Teams.Remove(team);
+        await _context.SaveChangesAsync();
+    }
+}
