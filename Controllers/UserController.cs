22d8085ecad946148b0bee0584ae2e4c75320fb0
Bogdan Kostadinov <bogdan.kostadinov1@gmail.com>
2025-06-13 00:10:03 +0100

    implementing user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using DotaNerf.DTOs;
+using DotaNerf.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotaNerf.Controllers;
+
+[ApiController]
+[Route("/users")]
+public class UserController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IUserRepository _userRepository;
+    public UserController(
+           IMapper mapper,
+           IUserRepository userRepository)
+    {
+        _mapper = mapper;
+        _userRepository = userRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsersAsync()
+    {
+        var users = await _userRepository.GetUsersAsync();
+
+        if (!users.Any())
+        {
+            return NotFound("No users found in the database.");
+        }
+
+        var userDtos = users.Select(user => _mapper.Map<UserDTO>(user)).ToList();
+
+        return Ok(userDtos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserAsync(Guid id)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var userDto = _mapper.Map<UserDTO>(user);
+        return Ok(userDto);
+    }
+}
+
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using DotaNerf.Data;
+using DotaNerf.Entities;
+using DotaNerf.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotaNerf.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly DataContext _context;
+
+    public UserRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<User>> GetUsersAsync()
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public Task<User?> GetUserByIdAsync(Guid id)
+    {
+        var user = _context.Users.AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Id == id);
+
+        return user ?? throw new KeyNotFoundException($"User with ID {id} not found.");
+    }
+}
