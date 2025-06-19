62353e2ace3761237e20fcd3a4c0d8d68bbd0f10
Boko <Boko@DESKTOP-CCQH80O>
2025-06-20 00:08:13 +0100

    unnecessary import

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -1,18 +1,15 @@
 ﻿using DotaNerf.Entities;
 using DotaNerf.Interfaces;
-using Microsoft.Extensions.Options;
 
 namespace DotaNerf.Services;
 
 public class AuthenticationService : IAuthenticationService
 {
     private readonly IUserRepository _userRepository;
-    private readonly IOptions<AuthenticationOptions> _authOptions;
 
-    public AuthenticationService(IUserRepository userRepository, IOptions<AuthenticationOptions> authOptions)
+    public AuthenticationService(IUserRepository userRepository)
     {
         _userRepository = userRepository;
-        _authOptions = authOptions;
     }
 
     public async Task<User?> AuthenticateAsync(string email, string password)
